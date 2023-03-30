3f27cbf03c3a886f87f610534d910762607d0c66
Julien Marcenac <julien.marcenac2@gmail.com>
2023-03-30 15:29:09 +0200

    add skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { SkillComponent } from './skill/skill.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      DeveloperComponent,
+      SkillComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
