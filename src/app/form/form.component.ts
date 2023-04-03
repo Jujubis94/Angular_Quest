f57db37662ed13afd780d34076da1448bd8cf392
Julien Marcenac <julien.marcenac2@gmail.com>
2023-04-03 14:57:40 +0200

    add comment

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,7 +7,7 @@ import { Onomatopoeia } from '../models/onomotoepiaList.model';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  model : Onomatopoeia = new Onomatopoeia('')
+  model : Onomatopoeia = new Onomatopoeia()
   constructor() { }
 
   ngOnInit() {
@@ -16,9 +16,20 @@ export class FormComponent implements OnInit {
   @Output() OnomatopiaToParent: EventEmitter<Onomatopoeia> = new EventEmitter(); 
   
   onSubmit() {
+    // premiere methode je déclare une nouvelle instance (case memoire) 
+    // avec la valeur du model initial puis je l'envoie
+
     const newOnomatopoeia = new Onomatopoeia(this.model.name);
     this.OnomatopiaToParent.emit(newOnomatopoeia);
     this.model.name = ''
+
+    // ou 
+    // this.model = new Onomatopoeia()
+
+
+    // seconde methode j'envoie le model puis je créé une nouvelle instance
+    // (fleche vers une nouvelle case mémoire) pour dissocier les valeurs de model
+    // this.OnomatopiaToParent.emit(this.model);
+    // this.model = new Onomatopoeia()
   }
-  
 }
--- src/app/models/onomotoepiaList.model.ts
+++ src/app/models/onomotoepiaList.model.ts
@@ -1,3 +1,6 @@
 export class Onomatopoeia {
-  constructor(public name: string) {}
+  // ? afin de pouvoir créer une nouvelle instance vide (undifined)
+  // possible aussi de mettre des valeurs par defaut 
+  // comme ça pas besoin de tout rentrer a la main a la création d'une nouvelle instance
+  constructor(public name?: string) {}
 }
